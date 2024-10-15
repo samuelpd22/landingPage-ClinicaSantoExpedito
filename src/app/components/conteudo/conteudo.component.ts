1f0dc5dac722d05e3fc834e7c85aeb2e31e6577e
samuelpd22 <samuel050101@hotmail.com>
2024-10-15 17:37:03 -0300

    feat: adc novo input

--- src/app/components/conteudo/conteudo.component.ts
+++ src/app/components/conteudo/conteudo.component.ts
@@ -1,27 +1,39 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Importe o FormsModule
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-conteudo',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './conteudo.component.html',
   styleUrl: './conteudo.component.scss'
 })
 export class ConteudoComponent {
 
   nome: string = '';
-  email: string = '';
+  medico: string = '';
   mensagem: string = '';
 
   enviarMensagem() {
-    const mensagemWhatsApp = `Olá, meu nome é ${this.nome}. Meu e-mail é ${this.email}. Mensagem: ${this.mensagem}`;
-    const numeroWhatsApp = '5599999999999'; // Altere para o número da clínica
+    // Encontre o médico correspondente ao ID selecionado
+    const medicoSelecionado = this.medicos.find(medico => medico.id === this.selectedMedico);
+    const medicoNome = medicoSelecionado ? medicoSelecionado.nome : 'não especificado';
+
+    const mensagemWhatsApp = `Olá, meu nome é ${this.nome}. Vaga para ${medicoNome}. Mensagem: ${this.mensagem}`;
+    const numeroWhatsApp = '5599999999999'; // Alterar para o número da clínica
     const url = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${encodeURIComponent(mensagemWhatsApp)}`;
     window.open(url, '_blank');
   }
 
 
- 
+  selectedMedico: string | null = null; // Para armazenar o médico selecionado
+  medicos = [
+    { id: '1', nome: 'Dr. Hélio de Carvalho' },
+    { id: '2', nome: 'Dr. Daniel Alvares' },
+    { id: '3', nome: 'Dr. Romeu dos Santos' },
+    // Adicione mais médicos conforme necessário
+  ];
 }
